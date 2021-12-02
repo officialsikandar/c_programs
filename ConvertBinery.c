d4016908bec6948c273cd2dd179c67ac3f95da0b
Sikandar Khan <84439796+officialsikandar@users.noreply.github.com>
2021-12-02 10:55:17 +0530

    update

--- ConvertBinery.c
+++ ConvertBinery.c
@@ -1,19 +1,19 @@
 #include<stdio.h>    
 #include<stdlib.h>  
-int main(){  
-int a[10],n,i;    
-system ("cls");  
-printf("Enter the number to convert: ");    
-scanf("%d",&n);    
-for(i=0;n>0;i++)    
+int main()
 {  
-  a[i]=n%2;    
-  n=n/2;    
-}    
-printf("\nBinary of Given Number is=");    
-for(i=i-1;i>=0;i--)    
-{
-  printf("%d",a[i]);    
-}    
-return 0;  
+  int arr[10],n,i;    
+  printf("Enter the Number for convert: ");    
+  scanf("%d",&n);    
+  for(i=0;n>0;i++)    
+  {  
+    arr[i]=n%2;    
+    n=n/2;    
+  }    
+  printf("\nBinary of Given Number is=");    
+  for(i=i-1;i>=0;i--)    
+  {
+    printf("%d",arr[i]);    
+  }    
+  return 0;  
 }  
