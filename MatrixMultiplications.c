b22e7511d72f01f6acd087e51be6e7b858e749a3
Sikandar Khan <84439796+officialsikandar@users.noreply.github.com>
2021-12-02 10:56:13 +0530

    Update

--- MatrixMultiplications.c
+++ MatrixMultiplications.c
@@ -1,59 +1,60 @@
 #include<stdio.h>    
 #include<stdlib.h>  
-int main(){  
-int am[10][10], bm[10][10], mul[10][10],row, col, i, j, k;    
+int main()
+{  
+    int am[10][10], bm[10][10], mul[10][10],row, col, i, j, k;    
 
-printf("Enter the number of row: \t");    
-scanf("%d",&row);    
-printf("Enter the number of column: \t");    
-scanf("%d",&col);   
+    printf("Enter the number of row: \t");    
+    scanf("%d",&row);    
+    printf("Enter the number of column: \t");    
+    scanf("%d",&col);   
 
-//input Values in Matrix
-printf("Enter the Matrix A Values: \n");    
-for(i=0;i<row;i++)    
-{
-    for(j=0;j<col;j++)    
+    //input Values in Matrix
+    printf("Enter the Matrix A Values: \n");    
+    for(i=0;i<row;i++)    
     {
-        scanf("%d",&am[i][j]);    
-    }    
-}   
-
-
-printf("Enter the Matrix B Values: \n");    
-for(i=0;i<row;i++)
-{
-    for(j=0;j<col;j++)    
-    {
-        scanf("%d",&bm[i][j]);    
-    }    
-}    
-   
-    
-//Calculate matrix multiply    
-printf("multiply of the matrix=\n");    
-for(i=0;i<row;i++)    
-{
-    for(j=0;j<col;j++)    
-    {
-        mul[i][j]=0;    
-        for(k=0;k<col;k++)    
+        for(j=0;j<col;j++)    
         {
-            mul[i][j]+=am[i][k]*bm[k][j];    
-        }
-    }    
-}   
+            scanf("%d",&am[i][j]);    
+        }    
+    }   
 
 
-//Output result    
-for(i=0;i<row;i++)    
-{   
-    for(j=0;j<col;j++)    
+    printf("Enter the Matrix B Values: \n");    
+    for(i=0;i<row;i++)
     {
-        printf("%d\t",mul[i][j]);    
-    }
-    printf("\n");    
-}  
+        for(j=0;j<col;j++)    
+        {
+            scanf("%d",&bm[i][j]);    
+        }    
+    }    
 
 
-return 0;  
+    //Calculate matrix multiply    
+    printf("multiply of the matrix=\n");    
+    for(i=0;i<row;i++)    
+    {
+        for(j=0;j<col;j++)    
+        {
+            mul[i][j]=0;    
+            for(k=0;k<col;k++)    
+            {
+                mul[i][j]+=am[i][k]*bm[k][j];    
+            }
+        }    
+    }   
+
+
+    //Output result    
+    for(i=0;i<row;i++)    
+    {   
+        for(j=0;j<col;j++)    
+        {
+            printf("%d\t",mul[i][j]);    
+        }
+        printf("\n");    
+    }  
+
+
+    return 0;  
 }  
